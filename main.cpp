694bf5fbf24e07d0710bc58186638c9753db37b8
Mryamfzli2003 <117342363+Mryamfzli2003@users.noreply.github.com>
2022-11-13 10:15:23 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+ main() {
+ 	char x;
+ 	cout<<"please enter a charactor:" ;
+ 	cin>>x;
+ 	switch(x){
+ 		case 'a':
+ 			cout<<"vowel";
+ 			break;
+ 		case 'i':
+		 	cout<<"vowel";
+		 	break;
+		case'e' :
+			cout<<"vowel";
+			break;
+		case'o'	:
+			cout<<"vowel";
+			break;
+		case'u'	:
+			cout<<"vowel";
+	 }
+	
+}
